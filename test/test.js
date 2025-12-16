63f096b047fa72ad1943c7d9df6e67901050d5dc
Joseph Huckaby <jhuckaby@pixlcore.com>
2025-12-16 09:18:52 -0800

    WIP: Add tests for tag API

--- test/test.js
+++ test/test.js
@@ -51,6 +51,7 @@ module.exports = {
 			require('./suites/test-plugins-api.js').tests,
 			require('./suites/test-secrets-api.js').tests,
 			require('./suites/test-roles-api.js').tests,
+			require('./suites/test-tags-api.js').tests,
 			require('./suites/test-servers-api.js').tests
 		);
 		
